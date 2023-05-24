e754ab9672549420984e35975aa31f954070c442
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-05-24 08:44:08 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() 
+{
+	int num1;
+	int num2;
+	int resultado;
+	
+	scanf ("%i",&num1);
+	scanf ("%i",&num2);
+	
+	resultado=num1+num2;
+	
+	printf("%i",resultado);
+
+	
+	
+	return 0;
+}
