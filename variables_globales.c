b2f4180c2168fe65626b81e7e94adac667eab5a5
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-05-29 08:06:21 -0600

    Add files via upload

---
+++ variables_globales.c
@@ -0,0 +1,23 @@
+/* programa numero 
+/* programa numero 
+titulo: variables globales
+fecha:
+Nombre:*/
+#include <stdio.h>
+int a,b,c ;                        //declaracion de variable global 
+int main()
+
+{
+int valor;                          //declaracion de varable local
+printf("tres valores");
+scanf ("%d %d %d\a",&a,&b,&c);      //a,b,c variables globales*  
+valor = a+b+c;
+printf("valor de a: %d\n"a);
+printf("valor de b: %d\n"b);
+printf("valor de c: %d\n"c);
+printf("suma de variables: %d" , valor);
+return 0;
+
+}
+
+
