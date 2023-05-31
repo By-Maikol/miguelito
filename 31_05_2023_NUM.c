ae264ad343daa2915cd375fdd4210d253394137d
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-05-31 07:58:01 -0600

    Add files via upload

---
+++ 31_05_2023_NUM.c
@@ -0,0 +1,13 @@
+/* programa numero
+titulo: imprime el tamaño de valores de coma flotante y doub
+fecha: 31_05_2023
+Nombre: Jose Miguel del Razo Rosales */
+#include <stdio.h>
+
+int main() {
+	printf ("el tamaño de variables de coma flotante es %d",sizeof (float));
+	printf ("bytes\n");
+	printf ("el tamaño de variables de doble presicion es %d", sizeof (double));
+	printf ("bytes\n");
+	return 0;
+}
