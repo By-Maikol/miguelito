49a55ae6ed8a7752e89b0ccb03e1794e61d7da8e
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-05-31 08:06:49 -0600

    Add files via upload

---
+++ 31_05_2023_ejercicio2.c
@@ -0,0 +1,26 @@
+/* programa numero
+titulo: imprime el tamaño de valores de coma flotante y doub
+fecha: 31_05_2023
+Nombre: Jose Miguel del Razo Rosales */
+#include <stdio.h>
+
+int main() {
+	int a,b,c;
+	int codigo, fahrenheit, coord_x, coord_y;
+	
+	codigo= 3467;
+	fahrenheit= 123.456;
+	coord_x=525;
+	coord_y=45;
+	a=(b=(c=45));
+	
+	
+	printf("%i valor de la variable 'codigo' \n",codigo);
+	printf("%i valor de la variable 'fahrenheit' \n",fahrenheit);
+	printf("%i valor de la variable 'coord_x' \n",coord_x);
+	printf("%i valor de la variable 'coord_y' \n",coord_y);
+	printf("%i valor de la variable 'a' \n",a);
+	printf("%i valor de la variable 'b' \n",b);
+	printf("%i valor de la variable 'c' \n",c);
+	return 0;
+}
