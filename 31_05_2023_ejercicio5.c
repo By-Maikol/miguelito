8d40d009fda7b4501f4b43a5924df1ec6b5aa124
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-05-31 08:32:56 -0600

    Add files via upload

---
+++ 31_05_2023_ejercicio5.c
@@ -0,0 +1,16 @@
+/* programa numero
+titulo: ejercicio 5
+fecha: 31_05_2023
+Nombre: Jose Miguel del Razo Rosales */
+#include <stdio.h>
+
+int main() 
+{
+	int n=5,t;
+	t= ++n * --n;
+	printf("n= %d, t=%d\n", n , t );
+	printf("%d %d %d\n", ++n , ++n ,++n) ;
+ 
+	return 0;
+	
+}
