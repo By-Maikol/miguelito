92423871e771bd5072a945da87721e625f1829e9
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-06-06 09:59:46 -0600

    Add files via upload

---
+++ 06ju2023n4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*estructura 'while'
+
+02/jun/2023 act4
+jose miguel*/
+int main()
+{
+  int contador = 1; 
+  while (contador>100)
+  {
+      printf ("%d\n", contador);
+      contador++;
+  }
+    return 0;
+}
