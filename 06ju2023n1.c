79ae7660b0e552b31bd1e5eddfe32db9a7b83590
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-06-06 09:48:58 -0600

    Add files via upload

---
+++ 06ju2023n1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*estructura basica de 'while'
+
+02/jun/2023 act1
+
+jose miguel*/
+int main()
+{
+    int x=0;
+    while(x<10)
+    printf("x:%d\n", x++);
+
+    return 0;
+}
+
