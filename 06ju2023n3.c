31cc639cc8c09219d87c5a99e3346a5464436561
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-06-06 09:53:57 -0600

    Add files via upload

---
+++ 06ju2023n3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*estructura 'while'
+
+02/jun/2023 act3
+
+Jose Miguel */
+int main()
+{
+  int contador = 0; //inicializo varible
+  while (contador<5)//prueba logica
+  
+  {
+      contador++; //si verdadro realiza bloque de instrucciones
+      printf ("contador: %d \n",contador);
+      
+  }
+    printf ("terminando el contador: %d \n",contador);
+    return 0;
+}
