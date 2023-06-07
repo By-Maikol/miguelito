e6d1171fc0ea3e860506ffbc57bd5b5048b6ddde
By-Maikol <132929247+By-Maikol@users.noreply.github.com>
2023-06-07 12:00:21 -0600

    Add files via upload

---
+++ 07_06_2023.c
@@ -0,0 +1,15 @@
+/* titulo
+fecha:07_06_2023
+nombre:miguel
+ */
+ #include <stdio.h>
+ 
+int main () 
+{ 
+    int i;
+    for (i = 1; i < 10; i++){
+	printf ("hola\n");
+    }
+    
+    return 0;
+}
